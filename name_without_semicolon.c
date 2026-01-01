e5ddfabdd8319c7dfa6ea438c8da00f9a0097738
Preeti Pragyan Mishra <preetipmishra2@gmail.com>
2026-01-01 14:15:51 +0530

    Print name without semicolon in C
    Implemented a method to print a name without using a semicolon in C by leveraging the printf function within an if statement.

---
+++ name_without_semicolon.c
@@ -0,0 +1,9 @@
+// printing name without using semicolon in program
+
+#include <stdio.h>       
+void main (void)
+{
+if (printf("Preeti Pragyan Mishra"))   //conditional statement to print the name, it returns 'true' as default
+{ }                       //empty if body
+}  
+//end of program
